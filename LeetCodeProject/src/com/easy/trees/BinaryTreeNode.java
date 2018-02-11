b83ba26773db188d012bb1c72373cc8ee7996080
Sricheta Ruj <srichetaruj.iste@gmail.com>
2018-02-11 15:12:01 -0800

    Trees

---
+++ LeetCodeProject/src/com/easy/trees/BinaryTreeNode.java
@@ -0,0 +1,28 @@
+package com.easy.trees;
+
+public class BinaryTreeNode {
+	
+	private int data;
+	private BinaryTreeNode left,right;
+	
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+	
+
+}
---
+++ LeetCodeProject/src/com/easy/trees/RecursivePreOrderTraversal.java
@@ -0,0 +1,5 @@
+package com.easy.trees;
+
+public class RecursivePreOrderTraversal {
+
+}
